853dde85f4f0e2ffa08e96a5e3787626d5a79cd1
riyabhanshali81 <150077318+riyabhanshali81@users.noreply.github.com>
2024-05-21 13:44:14 +0530

    Day2
    Use scanner class

--- DemoArea/ObjOfArea.java
+++ DemoArea/ObjOfArea.java
@@ -1,12 +1,24 @@
 package DemoArea;
+import java.util.Scanner;
 
 public class ObjOfArea {
 	public static void main(String[] args) {
         // Creating an object of the Area class
         Area rectangle = new Area();
 
+        //Value Form User
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Enter First Value:");
+
+        double f = s.nextDouble();
+        
+        System.out.println("Enter Secound Value:");
+
+        double se = s.nextDouble();
+        
         // Setting dimensions of the rectangle
-        rectangle.setDim(15.0,13.0);
+        rectangle.setDim(f,se);
 
         // Getting and printing the area of the rectangle
         System.out.println("Area of the rectangle: " + rectangle.getArea());
